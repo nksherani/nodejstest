a31aff0fe1dc5f3e76a7dc986018689a094fc2f8
naveed <nksherani@outlook.com>
2019-03-26 07:11:37 -0700

    no message

--- demoapp/src/App.js
+++ demoapp/src/App.js
@@ -3,11 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 class App extends Component {
+  state = {
+    customers: []
+  }
   render() {
     axios.get(`http://localhost:3000`)
       .then(res => {
-        const persons = res.data;
-        this.setState({ persons });
+        const customers = res.data;
+        this.setState({ customers });
       })
 
     return (
